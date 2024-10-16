1d677347fdaa840eb8dfcf78aac47c188838e0aa
jgarcia <jgarcia@107-04>
2024-10-16 10:50:23 +0200

    Creado el Acceso a Ficheros

---
+++ src/ejemplosconfile/VerInf.java
@@ -0,0 +1,23 @@
+package ejemplosconfile;
+
+import java.io.File;
+
+public class VerInf {
+	
+	public static void main(String[] args) {
+		System.out.println("INFORMACIÓN SOBRE EL FICHERO:");
+		  File f = new File("C:\\Users\\jgarcia\\eclipse-workspace\\AccesoAFicheros\\src\\ejemplosconfile\\VerInf.java");
+		   if(f.exists()){
+		        System.out.println("Nombre del fichero  : "+f.getName());
+		        System.out.println("Ruta                : "+f.getPath());
+		        System.out.println("Ruta absoluta       : "+f.getAbsolutePath());
+		        System.out.println("Se puede leer       : "+f.canRead());
+		        System.out.println("Se puede escribir   : "+f.canWrite());
+		        System.out.println("Tamaño              : "+f.length());
+				System.out.println("Es un directorio    : "+f.isDirectory()); 
+				System.out.println("Es un fichero       : "+f.isFile());
+				System.out.println("Nombre del directorio padre: "+f.getParent());
+		  }
+		 }
+		}
+
