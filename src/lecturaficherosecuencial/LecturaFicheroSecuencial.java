c0cb052f9a94b91afcb1ae1c7bb3fa9a89c304d9
jgarcia <jgarcia@107-04>
2024-10-23 11:13:11 +0200

    Le estás pillando el truco al tercero

---
+++ src/lecturaficherosecuencial/LecturaFicheroSecuencial.java
@@ -0,0 +1,59 @@
+package lecturaficherosecuencial;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LecturaFicheroSecuencial {
+
+	public static void main(String[] args) {
+
+		File archivoCreado;
+
+		// Archivo .txt que leeremos
+		String textoPalabras = "C:\\Users\\jgarcia\\eclipse-workspace\\AccesoAFicheros\\src\\lecturaficherosecuencial\\palabras.txt";
+
+		// Declaracion del BufferedReader
+		BufferedReader br;
+
+		// Cada una de las lineas leidas del .txt
+		String linea = "";
+
+		String nuevaLinea = "";
+
+		try {
+
+			br = new BufferedReader(new FileReader(textoPalabras));
+
+			linea = br.readLine();
+
+			while (linea != null) {
+
+				for (int i = 1; i < linea.length(); i++) {
+					if (Character.isUpperCase(linea.charAt(i))) {
+
+						nuevaLinea = linea.substring(0, i);
+
+						System.out.println(nuevaLinea);
+						break;
+					}
+				}
+
+				linea = br.readLine();
+			}
+
+			br.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
