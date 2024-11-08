80a61e377b056539acaa97a953462ea7ae863cf1
jgarcia <jgarcia@107-04>
2024-11-08 09:43:02 +0100

    Los txt donde corresponden

---
+++ src/ejerciciosaleatorios/segundo/EjercicioB.java
@@ -0,0 +1,10 @@
+package ejerciciosaleatorios.segundo;
+
+public class EjercicioB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ejerciciosaleatorios/tercero/EjercicioC.java
@@ -0,0 +1,10 @@
+package ejerciciosaleatorios.tercero;
+
+public class EjercicioC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
